3248b9e3fae3470e708defa969923673f4658d32
Franck Chapelon <franckick2@gmail.com>
2024-09-17 07:54:34 +0200

    commit 9

--- src/app/projets/page.tsx
+++ src/app/projets/page.tsx
@@ -3,89 +3,77 @@
 import Link from "next/link";
 import { projects } from "../data/data";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import Digits from "../components/digits";
+import { useMotionValue, motion, useSpring, MotionValue } from "framer-motion";
+import React, { useRef, useState } from "react";
 
 export default function Projets() {
+
+  const [hoveredProject, setHoveredProject] = useState<string | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const mouseXSpring = useSpring(x);
+  const mouseYSpring = useSpring(y);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = containerRef.current?.getBoundingClientRect();
+    if (rect) {
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+      x.set(mouseX / rect.width - 0.5);
+      y.set(mouseY / rect.height - 0.5);
+    }
+  };
 
   return (
     <section
       id="projets"
+      ref={containerRef}
+      onMouseMove={handleMouseMove}
       className="relative flex flex-col items-center justify-center min-h-screen w-full bg-stone-200 py-16"
     >
       <h2 className="text-4xl font-bold mb-8">Projets Sélectionnés</h2>
-      <div className="w-full">
+      <div className="w-full z-10">
         {projects.map((project) => (
           <motion.div
             key={project.id}
             className="group relative border-b border-black cursor-pointer overflow-hidden bg-white"
+            onMouseEnter={() => setHoveredProject(project.id)}
+            onMouseLeave={() => setHoveredProject(null)}
             whileHover="hover"
           >
             <motion.div
               className="absolute inset-0 bg-stone-200"
               variants={{
-                hover: { y: "100%" }
+                hover: { y: "100%" },
               }}
               transition={{ duration: 0.5, ease: "easeInOut" }}
             />
             <div className="relative">
               <Link href={project.link}>
-                <div className="flex items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mx-auto">
+                <div className="flex items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mx-auto w-full">
                   <div className="relative flex items-center justify-between w-full">
-                    <div className="relative inline-flex items-center justify-center w-32 h-24 overflow-hidden">
-                      {project.id.split('').map((digit, index) => (
-                        <motion.span
-                          key={`top-${index}`}
-                          className="font-serif tracking-tighter text-8xl font-bold inline-block absolute leading-snug"
-                          style={{ left: `${index * 45}%` }}  // Ajusté le positionnement horizontal
-                          initial={{ y: "200%" }}
-                          animate={{ y: 0 }}
-                          transition={{ delay: index * 0.1, duration: 0.5 }}
-                          variants={{
-                            hover: { y: "-100%" }
-                          }}
-                        >
-                          {digit}
-                        </motion.span>
-                      ))}
-                      {project.id.split('').map((digit, index) => (
-                        <motion.span
-                          key={`bottom-${index}`}
-                          className="font-pinyon-script text-8xl font-bold inline-block absolute"
-                          style={{ left: `${index * 40}%` }}  // Ajusté le positionnement horizontal
-                          initial={{ y: "200%" }}
-                          variants={{
-                            hover: { y: 0 }
-                          }}
-                          transition={{ delay: index * 0.1, duration: 0.5 }}
-                        >
-                          {digit}
-                        </motion.span>
-                      ))}
+                    <div className="leading-snug py-2 h-full flex items-center justify-start group-hover:translate-x-12 transition-transform duration-500">
+                      <Digits id={project.id} />
+                      <div className="">
+                        <h3 className="text-3xl font-inter ml-20">
+                          {project.name}
+                        </h3>
+                      </div>
                     </div>
 
-                    <div className="">
-                      <span className="text-6xl font-inter ml-20">
-                        {project.name}
-                      </span>
-                    </div>
+                    <ImageProjets
+                      image={project.image}
+                      name={project.name}
+                      isHovered={hoveredProject === project.id}
+                      mouseX={mouseXSpring}
+                      mouseY={mouseYSpring}
+                    />
 
-                    <div className="relative w-96 h-48 overflow-hidden">
-                      <Image
-                        src={project.image}
-                        alt={project.name}
-                        fill
-                        className="object-cover transition-transform duration-500 group-hover:scale-110"
-                      />
-                    </div>
                   </div>
-                  <motion.span
-                    className="text-2xl"
-                    variants={{
-                      hover: { x: 10 }
-                    }}
-                  >
-                    →
-                  </motion.span>
+                  <span className="text-5xl group-hover:-translate-x-6 transition-transform duration-500">→</span>
                 </div>
               </Link>
             </div>
@@ -103,3 +91,42 @@ export default function Projets() {
     </section>
   );
 }
+
+const ImageProjets = ({image, name, isHovered, mouseX, mouseY}: {
+  image: string, 
+  name: string, 
+  isHovered: boolean,
+  mouseX: MotionValue<number>,
+  mouseY: MotionValue<number>
+}) => {
+  return (
+    <motion.div 
+      className="absolute right-0 top-0 w-[24vw] h-[18vh] rounded-full overflow-hidden z-50"
+      initial={{ scale: 0, x: 50, opacity: 0 }}
+      animate={{ scale: isHovered ? 1 : 0, x: isHovered ? 0 : 50, opacity: isHovered ? 1 : 0 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+    >
+      <motion.div
+        className="absolute w-[24vw] h-[18vh] rounded-full overflow-hidden"
+        style={{
+          top: "50%",
+          left: "50%",
+          x: mouseX,
+          y: mouseY,
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        initial={{ scale: 0 }}
+        animate={{ scale: isHovered ? 1 : 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        <Image 
+          src={image} 
+          alt={name} 
+          fill 
+          className="object-cover" 
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
